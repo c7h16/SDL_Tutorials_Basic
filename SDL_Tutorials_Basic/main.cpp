0c76fd673a76a4e5568e0610db04156eecd3c09a
Developer <developer@macbook-pro-2.local>
2018-03-16 19:04:53 -0700

    Initial Commit

---
+++ SDL_Tutorials_Basic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_Tutorials_Basic
+//
+//  Created by Developer on 3/16/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
